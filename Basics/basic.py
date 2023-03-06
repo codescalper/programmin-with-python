2df487bc798cba3d1297643bea21614687fe764f
codescalper <msfunbook@gmail.com>
2023-03-06 15:40:39 +0530

    Did basic of python

---
+++ Basics/basic.py
@@ -0,0 +1,26 @@
+print("Hello ");
+
+print("World")
+
+a = 1+            2;
+b = 1+            2 \
++3
+print(a)
+print(b)
+
+# del a       #Used to delete the variable.
+
+print(a)
+
+# input and output 
+
+
+# By default input will give us type string 
+name = input("Please enter your name: ")
+print('Your name is ',name)
+
+num1=input('Enter a number: ')
+num2=input('Enter a number: ')
+
+print(int(num1)+int(num2))
+
